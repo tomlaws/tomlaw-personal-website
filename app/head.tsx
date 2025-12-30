b2f43fd3080c35f01efd0e4d863859db91766889
Tom Law <lawhowang@icloud.com>
2025-12-30 16:31:36 +0800

    update job title from software engineer to backend developer

--- app/head.tsx
+++ app/head.tsx
@@ -3,11 +3,11 @@ export default function Head() {
     <>
       <title>Tom Law</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm a software engineer based in HK 😀." />
+      <meta name="description" content="I'm a backend developer based in HK 😀." />
       <meta property="og:title" content="Tom Law" />
       <meta property="og:url" content="https://tomlaw.me/" />
       <meta property="og:image" content="https://avatars.githubusercontent.com/u/47745140?v=5" />
-      <meta property="og:description" content="I'm a software engineer based in HK 😀." />
+      <meta property="og:description" content="I'm a backend developer based in HK 😀." />
       <meta property="og:type" content="profile" />
       <meta property="og:profile:first_name" content="Tom" />
       <meta property="og:profile:last_name" content="Law" />
