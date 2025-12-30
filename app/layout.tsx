6c8105140667554468b9d50dd30e669d0907a7f6
Tom Law <lawhowang@icloud.com>
2025-12-30 16:33:21 +0800

    add Head component to layout for improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from './head'
 
 export default function RootLayout({
   children,
@@ -11,7 +12,9 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head >
+        <Head />
+      </head>
       <body>{children}</body>
     </html>
   )
