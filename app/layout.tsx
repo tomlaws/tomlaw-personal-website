73f58458c36d3dd451f3ad7adcbefef1985c525f
Tom Law <lawhowang@icloud.com>
2025-12-30 16:34:17 +0800

    move Head component content directly into layout for improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css'
-import Head from './head'
 
 export default function RootLayout({
   children,
@@ -12,8 +11,19 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head >
-        <Head />
+      <head>
+        <title>Tom Law</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="I'm a backend developer based in HK 😀." />
+        <meta property="og:title" content="Tom Law" />
+        <meta property="og:url" content="https://tomlaw.me/" />
+        <meta property="og:image" content="https://avatars.githubusercontent.com/u/47745140?v=5" />
+        <meta property="og:description" content="I'm a backend developer based in HK 😀." />
+        <meta property="og:type" content="profile" />
+        <meta property="og:profile:first_name" content="Tom" />
+        <meta property="og:profile:last_name" content="Law" />
+        <meta property="og:profile:username" content="tomlaw" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
     </html>
